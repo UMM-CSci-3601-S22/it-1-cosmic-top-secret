bb0fb3f4f0cb4031d746b603e9ea2cf9781984b5
Shua <enge0479@morris.umn.edu>
2022-03-03 15:11:57 -0600

    Implement and write tests for item.service
    includes a service that adds an item

--- client/src/app/items/item.service.ts
+++ client/src/app/items/item.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Item } from './item';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ItemService {
+  readonly itemUrl: string = environment.apiUrl + 'items';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getItems(): Observable<Item[]> {
+    const httpParams: HttpParams = new HttpParams();
+    return this.httpClient.get<Item[]>(this.itemUrl, {
+      params: httpParams,
+    });
+  }
+
+  addItem(newItem: Item): Observable<string> {
+    // Send post request to add a new item with the item data as the body.
+    return this.httpClient.post<{id: string}>(this.itemUrl, newItem).pipe(map(res => res.id));
+  }
 }
