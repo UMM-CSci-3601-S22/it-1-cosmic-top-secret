2ce6bb1640922362833cf20a37d0cb7ef16d14ff
Joshua Engelkes <enge0479@morris.umn.edu>
2022-03-05 14:16:52 -0600

    Implement and test item.profile component

--- client/src/app/items/item-profile.component.spec.ts
+++ client/src/app/items/item-profile.component.spec.ts
@@ -1,17 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
+import { MockItemService } from '../../testing/item.service.mock';
+import { Item } from './item';
+import { ItemCardComponent } from './item-card.component';
 import { ItemProfileComponent } from './item-profile.component';
+import { ItemService } from './item.service';
 
 describe('ItemProfileComponent', () => {
   let component: ItemProfileComponent;
   let fixture: ComponentFixture<ItemProfileComponent>;
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ItemProfileComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MatCardModule
+      ],
+      declarations: [ItemProfileComponent, ItemCardComponent],
+      providers: [
+        { provide: ItemService, useValue: new MockItemService() },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemProfileComponent);
@@ -19,7 +35,44 @@ describe('ItemProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to a specific item profile', () => {
+    const expectedItem: Item = MockItemService.testItems[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `ItemProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedItem._id });
+
+    expect(component.id).toEqual(expectedItem._id);
+    expect(component.item).toEqual(expectedItem);
+  });
+
+  it('should navigate to correct item when the id parameter changes', () => {
+    let expectedItem: Item = MockItemService.testItems[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `ItemProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedItem._id });
+
+    expect(component.id).toEqual(expectedItem._id);
+
+    // Changing the paramMap should update the displayed item profile.
+    expectedItem = MockItemService.testItems[1];
+    activatedRoute.setParamMap({ id: expectedItem._id });
+
+    expect(component.id).toEqual(expectedItem._id);
+  });
+
+  it('should have `null` for the item for a bad ID', () => {
+    activatedRoute.setParamMap({ id: 'badID' });
+
+    // If the given ID doesn't map to a Item, we expect the service
+    // to return `null`, so we would expect the component's item
+    // to also be `null`.
+    expect(component.id).toEqual('badID');
+    expect(component.item).toBeNull();
   });
 });
--- client/src/app/items/item-profile.component.ts
+++ client/src/app/items/item-profile.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from './item';
+import { ItemService } from './item.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-item-profile',
   templateUrl: './item-profile.component.html',
   styleUrls: ['./item-profile.component.scss']
 })
-export class ItemProfileComponent implements OnInit {
+export class ItemProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  item: Item;
+  id: string;
+  getItemSub: Subscription;
+
+  constructor(private route: ActivatedRoute, private ItemService: ItemService) { }
 
   ngOnInit(): void {
+    // We subscribe to the parameter map here so we'll be notified whenever
+    // that changes (i.e., when the URL changes) so this component will update
+    // to display the newly requested Item.
+    this.route.paramMap.subscribe((pmap) => {
+      this.id = pmap.get('id');
+      if (this.getItemSub) {
+        this.getItemSub.unsubscribe();
+      }
+      this.getItemSub = this.ItemService.getItemById(this.id).subscribe(Item => this.item = Item);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.getItemSub) {
+      this.getItemSub.unsubscribe();
+    }
   }
 
 }
