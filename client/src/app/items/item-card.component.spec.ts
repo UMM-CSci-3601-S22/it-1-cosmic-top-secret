ed32b7dafd63f5834af109c1136d714d698fd46c
Joshua Engelkes <enge0479@morris.umn.edu>
2022-03-05 13:32:15 -0600

    Finish creation of item-card component by adding tests and design

--- client/src/app/items/item-card.component.spec.ts
+++ client/src/app/items/item-card.component.spec.ts
@@ -1,21 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ItemCardComponent } from './item-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 describe('ItemCardComponent', () => {
   let component: ItemCardComponent;
   let fixture: ComponentFixture<ItemCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        MatCardModule
+      ],
       declarations: [ ItemCardComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemCardComponent);
     component = fixture.componentInstance;
+    component.item = {
+      name: 'Granny Smith Apples',
+    };
     fixture.detectChanges();
   });
 
